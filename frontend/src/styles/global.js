c5a4de2ef38096942c6a95d5d6232ec3d5365279
Carlos Eduardo Drago <75817277+Caduber@users.noreply.github.com>
2025-01-27 02:52:59 -0300

    Add files via upload

---
+++ frontend/src/styles/global.js
@@ -0,0 +1,19 @@
+import {createGlobalStyle} from "styled-components"
+
+const Global = createGlobalStyle`
+
+*{
+    margin: 0px;
+    padding: 0px;
+    font-family: "Montserrat", "Arial";
+}
+
+body{
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    background-conlor: #f0f0f0;
+}
+`
+
+export default Global
